b0946e6a6e01b71c34d426a14d54a3995242604f
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-07-20 12:02:48 +0200

    Updated firebase config values

--- config/firebase.js
+++ config/firebase.js
@@ -1,15 +1,16 @@
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 // Firebase config
 const firebaseConfig = {
-    apiKey: "AIzaSyC4Mks67a5pf5KN23qsYi7cGLJvsgWBD0g",
-    authDomain: "meet-app-352610.firebaseapp.com",
-    projectId: "meet-app-352610",
-    storageBucket: "meet-app-352610.appspot.com",
-    messagingSenderId: "590789825703",
-    appId: "1:590789825703:web:a31eb550ca5bc62f743782"
-  };
+  apiKey: "AIzaSyC4Mks67a5pf5KN23qsYi7cGLJvsgWBD0g",
+  authDomain: "meet-app-352610.firebaseapp.com",
+  projectId: "meet-app-352610",
+  storageBucket: "meet-app-352610.appspot.com",
+  messagingSenderId: "590789825703",
+  appId: "1:590789825703:web:a31eb550ca5bc62f743782"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
